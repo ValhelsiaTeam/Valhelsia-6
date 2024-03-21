aa35274a32dc87c94777d497cc02fb3ff3a49032
Vaelzan <vaelzan@evilcult.net>
2024-03-22 02:16:08 +1100

    Update entity_type_tags.js

--- kubejs/server_scripts/tags/entity_type_tags.js
+++ kubejs/server_scripts/tags/entity_type_tags.js
@@ -15,6 +15,36 @@
  */
 ServerEvents.tags('entity_type', event => {
 
+  // Blacklist some overpowered drygmy mobs (mainly bosses).
+  event.add('ars_nouveau:drygmy_blacklist', [
+    //'irons_spellbooks:dead_king',
+    'twilightforest:alpha_yeti',
+    'twilightforest:hydra',
+    'twilightforest:lich',
+    'twilightforest:naga',
+    'twilightforest:snow_queen',
+    'twilightforest:ur_ghast',
+  ]);
+
+  // Expand Ars Nouveau Magic Find to other mobs.
+  event.add('ars_nouveau:magic_find', [
+    // TODO: Go through Aether, Malum, etc. for more.
+    'artifacts:mimic',
+    'conjurer_illager:conjurer',
+    //'irons_spellbooks:archevoker',
+    //'irons_spellbooks:cryomancer',
+    //'irons_spellbooks:dead_king',
+    //'irons_spellbooks:necromancer',
+    //'irons_spellbooks:priest',
+    //'irons_spellbooks:pyromancer',
+    'minecraft:evoker',
+    'minecraft:illusioner',
+    'twilightforest:death_tome',
+    'twilightforest:lich',
+    'twilightforest:skeleton_druid',
+    'twilightforest:wraith',
+  ]);
+
   // Add Halloween Event Candy to Spoopy Mobs
   event.add('valhelsia_tweaks:drops_candy', [
     'minecraft:zombie',
@@ -26,14 +56,27 @@ ServerEvents.tags('entity_type', event => {
     'minecraft:phantom',
     'minecraft:cave_spider',
     'minecraft:wither_skeleton',
-    'alexsmobs:underminer',
+    'alexsmobs:bone_serpent',
     'alexsmobs:murmur',
-    //'quark:wraith',
-    //'quark:forgotten',
+    'alexsmobs:skelewag',
+    'alexsmobs:underminer',
+    'aquamirae:captain_cornelia',
+    'aquamirae:tortured_soul',
+    //'environmental:zombie_deer',
     'goblintraders:goblin_trader',
     'goblintraders:vein_goblin_trader',
+    'minecolonies:mummy',
+    'minecolonies:pharao',
+    'quark:forgotten',
+    'quark:wraith',
     //'savage_and_ravage:skeleton_villager',
+    'twilightforest:death_tome',
+    'twilightforest:hedge_spider',
+    'twilightforest:king_spider',
+    'twilightforest:lich',
     'twilightforest:skeleton_druid',
+    'twilightforest:swarm_spider',
+    'twilightforest:wraith',
   ]);
 
 });
