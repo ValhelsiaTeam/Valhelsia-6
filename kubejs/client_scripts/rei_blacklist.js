6a810f052b6929c71ea2e3d518f69fea0d184993
Vaelzan <vaelzan@evilcult.net>
2024-04-13 22:59:03 +1000

    Blacklist a bunch of dev / internal items from displaying in REI.

--- kubejs/client_scripts/rei_blacklist.js
+++ kubejs/client_scripts/rei_blacklist.js
@@ -94,6 +94,7 @@ REIEvents.hide('item', event => {
 
     // Create
     'create:copper_backtank_placeable',
+    'create:netherite_backtank_placeable',
 
     // Darker Depths
     'darkerdepths:silver_ore',
@@ -102,6 +103,11 @@ REIEvents.hide('item', event => {
     'darkerdepths:silver_ingot',
     'darkerdepths:silver_block',
     'darkerdepths:rope',
+
+    // Deeper and Darker
+    'deeperdarker:sculk_tendrils_plant',
+    'deeperdarker:sculk_vines_plant',
+    'deeperdarker:otherside_portal',
 
     // Domum Ornamentum
     'domum_ornamentum:plain',
@@ -145,6 +151,57 @@ REIEvents.hide('item', event => {
     'embers:lead_ore',
     'embers:deepslate_lead_ore',
 
+    // Enlightend
+    'enlightened_end:cerulean_vine_tip',
+    'enlightened_end:cerulean_bush_top',
+    'enlightened_end:cerulean_bush_bottom',
+    'enlightened_end:helium_gas',
+    'enlightened_end:xenon_gas',
+    'enlightened_end:planetary_air',
+    'enlightened_end:plantery_system_air',
+    'enlightened_end:springleaf_vine',
+    'enlightened_end:spring_leaf_deactivated',
+    'enlightened_end:laser_emission',
+    'enlightened_end:closed_malachite_capsule',
+    'enlightened_end:malachite_reflector_up',
+    'enlightened_end:malachite_reflector_horizontal',
+    'enlightened_end:malachite_reflector_veritcal_on', // typo in mod
+    'enlightened_end:malachite_reflector_horizontal_on',
+    'enlightened_end:malachite_reflector_horizontal',
+    'enlightened_end:malachite_reflector_down',
+    'enlightened_end:malachite_reflector_down_on',
+    'enlightened_end:xenon_laser_on',
+    'enlightened_end:bouncling_nut_0',
+    'enlightened_end:bouncling_nut_2',
+    'enlightened_end:azure_berry_vine_0',
+    'enlightened_end:azure_berry_vine_1',
+    'enlightened_end:azure_berry_vine_2',
+    'enlightened_end:irradium_lamp_off',
+    'enlightened_end:gas_vent_middle',
+    'enlightened_end:gas_vent_ooze',
+    'enlightened_end:gas_vent_bottom',
+    'enlightened_end:gas_vent_water',
+    'enlightened_end:gas_vent_lava',
+    'enlightened_end:gas_vent_xenon',
+    'enlightened_end:gas_vent_helium',
+    'enlightened_end:cerulean_stalk_0',
+    'enlightened_end:cerulean_stalk_1',
+    'enlightened_end:cerulean_stalk_2',
+    'enlightened_end:cerulean_stalk_3',
+    'enlightened_end:elevibloom_0',
+    'enlightened_end:elevibloom_1',
+    'enlightened_end:elevibloom_2',
+    'enlightened_end:elevibloom_3',
+    'enlightened_end:ooze_cauldron_full',
+    'enlightened_end:ooze_cauldron_2',
+    'enlightened_end:ooze_cauldron_1',
+    'enlightened_end:ringling_pot',
+    'enlightened_end:potted_ennegel_fern',
+    'enlightened_end:potted_odd_roots',
+    'enlightened_end:potted_open_odd_roots',
+    'enlightened_end:potted_indigo_sprouts',
+    'enlightened_end:potted_indigo_roots',
+
     // Every Compat
     'everycomp:all_woods',
 
@@ -179,9 +236,16 @@ REIEvents.hide('item', event => {
     'immersiveengineering:ingot_steel',
     'immersiveengineering:nugget_steel',
     'immersiveengineering:nugget_copper',
+    'immersiveengineering:fake_icon_birthday',
+    'immersiveengineering:fake_icon_lucky',
+    'immersiveengineering:fake_icon_drillbreak',
+    'immersiveengineering:fake_icon_ravenholm',
+    'immersiveengineering:fake_icon_fried',
+    'immersiveengineering:fake_icon_bttf',
 
     // Mekanism
     'mekanism:block_charcoal',
+    'mekanism:bounding_block',
     'mekanism:creative_fluid_tank',
     'mekanism:creative_chemical_tank',
     'mekanism:uranium_ore',
@@ -216,6 +280,13 @@ REIEvents.hide('item', event => {
     'mekanismtools:steel_leggings',
     'mekanismtools:steel_boots',
 
+    // Moonlight
+    'moonlight:placeable_item',
+
+    // Phantasmic (Nourished Nether)
+    'nourished_nether:basalt_ash_spawner',
+    'nourished_nether:ectoplasm_block_active',
+
     // Quark
     'quark:carrot_crate',
     'quark:potato_crate',
@@ -231,6 +302,14 @@ REIEvents.hide('item', event => {
     'structure_gel:cyan_gel',
     'structure_gel:orange_gel',
     'structure_gel:yellow_gel',
+
+    // Tetra
+    'tetra:modular_bow',
+    'tetra:modular_crossbow',
+    'tetra:modular_bow',
+    'tetra:modular_single',
+    'tetra:modular_shield',
+    'tetra:shootable_dummy',
 
     // Twilight Forest
     'twilightforest:uncrafting_table',
